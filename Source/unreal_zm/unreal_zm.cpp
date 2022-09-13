65e559afc4069e8b4325794c2d99c6ead7875748
nickhildebrant <nickhildebrant09@gmail.com>
2022-09-12 23:59:33 -0700

    Hello, World

---
+++ Source/unreal_zm/unreal_zm.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "unreal_zm.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, unreal_zm, "unreal_zm" );
---
+++ Source/unreal_zm/unreal_zmGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "unreal_zmGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_ZM_API Aunreal_zmGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
